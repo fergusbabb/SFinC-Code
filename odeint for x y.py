087aaf9f32f47a1bfcf27cdec6d24d1020bb1ecc
Felix <felix.black01@gmail.com>
2024-02-01 11:34:42 +0000

    change figure style, add indicators for fixed points

--- odeint for x y.py
+++ odeint for x y.py
@@ -4,6 +4,9 @@ from matplotlib import pyplot as plt
 
 lam = 1
 gam = 1
+
+plt.annotate("$\lambda = $" + str(lam) + "\n$\gamma = $" + str(gam), xy = (-1, 1))
+
 def vectorPrime(state, N):
     global lam
     global gam
@@ -16,17 +19,37 @@ def vectorPrime(state, N):
 
     return [xprime, yprime]
 
+
+if lam**2 > 3*gam:
+    x = np.sqrt(1.5) * gam/lam
+    y = np.sqrt(1.5 * (2 - gam) * gam/lam**2)
+
+elif lam**2 < 6:
+    x = lam/np.sqrt(6)
+    y = np.sqrt(1 - x**2)
+
+else:
+    x = 1
+    y = 0
+
+omegaDE = x**2 + y**2
+
+
 theta = np.linspace(0, np.pi, 51)
 unitCircleX = np.cos(theta)
 unitCircleY = np.sin(theta)
 
+omegaDEcircleX = np.sqrt(omegaDE) * unitCircleX
+omegaDEcircleY = np.sqrt(omegaDE) * unitCircleY
+
 plt.plot(unitCircleX, unitCircleY, color="r")
-plt.plot([0, np.sqrt(2)/2], [0, np.sqrt(2)/2], "r--")
+plt.plot(omegaDEcircleX, omegaDEcircleY, "r--", linewidth=0.75)
+plt.plot([0, x/np.sqrt(omegaDE)], [0, y/np.sqrt(omegaDE)], "r--", linewidth=0.75)
 
 N = np.linspace(0, 8, 401)
 
-for x0 in np.arange(-0.99, 1, 0.1):
-    for y0 in [0.01]:#np.arange(0, 1, 0.1):
+for x0 in np.arange(-0.99, 1, 0.2):
+    for y0 in np.arange(0.01, 1, 0.1):
         if x0**2 + y0**2 > 1:
             continue
         path = integrate.odeint(vectorPrime, [x0, y0], N)
@@ -35,7 +58,7 @@ for x0 in np.arange(-0.99, 1, 0.1):
         pathx = pathTranspose[0]
         pathy = pathTranspose[1]
 
-        plt.plot(pathx, pathy, color="b")
+        plt.plot(pathx, pathy, color="k", linewidth=0.25)
 
 plt.axis("equal")
 plt.show()
