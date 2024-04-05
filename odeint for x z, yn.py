180c6ece26503f7fbfbc29e8dbd58d5a6dee594c
Felix <felix.black01@gmail.com>
2024-04-05 21:33:02 +0100

    Implemented general multiple potentials
    To add another potential, add another element to lambda
    Current starting conditions is the total y0 is distributed evenly among all potential terms
    
    Still need to get fixed points.

--- odeint for x z, yn.py
+++ odeint for x z, yn.py
@@ -2,7 +2,17 @@ import numpy as np
 from scipy import integrate
 from matplotlib import pyplot as plt
 
-lam = 1
+lam = np.array([1, 3])
+
+## Initial conditions
+x0 = -0.01
+y0 = 0.01 / np.sqrt(len(lam))
+z0 = 0.9
+
+start = np.append(x0, np.append([y0 for lambda_n in lam], z0))
+
+print(np.sum(start**2))
+
 
 fig, ax = plt.subplots(2, 2)
 
@@ -14,16 +24,16 @@ def vectorPrime(state, N):
     global lam
     
     x = state[0]
-    y = state[1]
-    z = state[2]
+    y = state[1:-1]
+    z = state[-1]
 
-    HdotSection = 1 + x**2 - y**2 + (1/3)*z**2
+    HdotSection = 1 + x**2 - np.sum(y**2) + (1/3)*z**2
 
-    xprime = (-3*x) + (lam * np.sqrt(1.5) * y**2)  + (1.5*x * HdotSection)
-    yprime =         (-lam * np.sqrt(1.5) * x * y) + (1.5*y * HdotSection)
-    zprime = (-2*z)                                + (1.5*z * HdotSection)
+    xprime = (-3*x) + (np.sqrt(1.5) *  np.sum(lam * y**2)) + (1.5*x * HdotSection)
+    yprime =          (np.sqrt(1.5) *        -lam * x * y) + (1.5*y * HdotSection)
+    zprime = (-2*z)                                        + (1.5*z * HdotSection)
 
-    return [xprime, yprime, zprime]
+    return np.append(xprime, np.append(yprime, zprime))
 
 def accelerationExpression(x, y, z):
     HdotSection = 1 + x**2 - y**2 + (1/3)*z**2
@@ -54,16 +64,11 @@ ax[1, 0].plot([reverseN[-1], N[-1]], [1, 1], "k--", linewidth = 0.5)
 
 ## Initial conditions
 
-if lam > 2:
-    y0 = 2 / (lam * np.sqrt(3))#0.2
-    x0 = 2 * np.sqrt(2/3) / lam #np.sqrt(2) * y0
-    z0 = np.sqrt(1 - (4/lam**2)) - 0.005
-else:
-    x0 = -0.01#0.05
-    y0 = 0.01
-    z0 = 0.9
-
-print(x0**2 + y0**2 + z0**2)
+##if lam[0] > 2:
+##    y0 = 2 / (lam * np.sqrt(3))#0.2
+##    x0 = 2 * np.sqrt(2/3) / lam #np.sqrt(2) * y0
+##    z0 = np.sqrt(1 - (4/lam**2)) - 0.005
+##else:
 
 fixedPoints = np.array([
     [0, 0, 0],
@@ -72,49 +77,66 @@ fixedPoints = np.array([
     [-1, 0, 0]
     ])
 
-if lam**2 < 6:
-    fixedPoints = np.append(fixedPoints, [[lam/np.sqrt(6), np.sqrt(1 - (lam**2)/6), 0]], axis=0)
+##if lam**2 < 6:
+##    fixedPoints = np.append(fixedPoints, [[lam/np.sqrt(6), np.sqrt(1 - (lam**2)/6), 0]], axis=0)
+##
+##if lam**2 > 3:
+##    fixedPoints = np.append(fixedPoints, [[np.sqrt(3/2)/lam, np.sqrt(3/2)/lam, 0]], axis=0)
+##
+##if lam**2 > 8/3:
+##    fixedPoints = np.append(fixedPoints, [[2 * np.sqrt(2/3) / lam, 2 / (lam * np.sqrt(3)), np.sqrt(1 - (4/lam**2))]], axis=0)
 
-if lam**2 > 3:
-    fixedPoints = np.append(fixedPoints, [[np.sqrt(3/2)/lam, np.sqrt(3/2)/lam, 0]], axis=0)
+## FORWARD PATH
 
-if lam**2 > 8/3:
-    fixedPoints = np.append(fixedPoints, [[2 * np.sqrt(2/3) / lam, 2 / (lam * np.sqrt(3)), np.sqrt(1 - (4/lam**2))]], axis=0)
-
-path = integrate.odeint(vectorPrime, [x0, y0, z0], N)
+path = integrate.odeint(vectorPrime, start, N)
 pathTranspose = path.transpose()
 
-pathx = pathTranspose[0]
-pathy = pathTranspose[1]
-pathz = pathTranspose[2]
+pathx   = pathTranspose[0]
+pathy_n = pathTranspose[1:-1]
+pathz   = pathTranspose[-1]
 
-ax[0, 0].plot(pathx, pathy, pathz, linewidth=0.75)
+pathy_total = np.sqrt(np.sum(pathy_n**2, axis=0))
 
-ax[1, 0].plot(N, gamma_phi(pathx, pathy))
+## PLOT FORWARD
 
-ax[1, 1].plot(N, pathz**2,                           "r", label = "$\Omega_r = z^2$")
-ax[1, 1].plot(N, 1 - pathx**2 - pathy**2 - pathz**2, "g", label = "$\Omega_m = 1 - x^2 - y^2 - z^2$")
-ax[1, 1].plot(N, pathx**2 + pathy**2,                "b", label = "$\Omega_\phi = x^2 + y^2$")
+ax[0, 0].plot(pathx, pathy_total, pathz, linewidth=0.75)
+ax[1, 0].plot(N, gamma_phi(pathx, pathy_total))
 
-ax[0, 1].plot(N, accelerationExpression(pathx, pathy, pathz))
+for pathy in pathy_n:
+    ax[1, 1].plot(N, pathy**2, "b--", linewidth=0.5)
 
+ax[1, 1].plot(N, pathz**2,                                 "r", label = "$\Omega_r = z^2$")
+ax[1, 1].plot(N, 1 - pathx**2 - pathy_total**2 - pathz**2, "g", label = "$\Omega_m = 1 - x^2 - y^2 - z^2$")
+ax[1, 1].plot(N, pathx**2 + pathy_total**2,                "b", label = "$\Omega_\phi = x^2 + y^2$")
 
-reversePath = integrate.odeint(vectorPrime, [x0, y0, z0], reverseN)
+ax[0, 1].plot(N, accelerationExpression(pathx, pathy_total, pathz))
+
+## REVERSE PATH
+
+reversePath = integrate.odeint(vectorPrime, start, reverseN)
 reversePathTranspose = reversePath.transpose()
 
 reversePathx = reversePathTranspose[0]
-reversePathy = reversePathTranspose[1]
-reversePathz = reversePathTranspose[2]
+reversePathy_n = reversePathTranspose[1:-1]
+reversePathz = reversePathTranspose[-1]
 
-ax[0, 0].plot(reversePathx, reversePathy, reversePathz, linewidth=0.75)
+reversePathy_total = np.sqrt(np.sum(reversePathy_n**2, axis=0))
 
-ax[1, 0].plot(reverseN, gamma_phi(reversePathx, reversePathy))
+## PLOT REVERSE
 
-ax[1, 1].plot(reverseN, reversePathz**2,                           "r")
-ax[1, 1].plot(reverseN, 1 - reversePathx**2 - reversePathy**2 - reversePathz**2, "g")
-ax[1, 1].plot(reverseN, reversePathx**2 + reversePathy**2,                "b")
+ax[0, 0].plot(reversePathx, reversePathy_total, reversePathz, linewidth=0.75)
+ax[1, 0].plot(reverseN, gamma_phi(reversePathx, reversePathy_total))
 
-ax[0, 1].plot(reverseN, accelerationExpression(reversePathx, reversePathy, reversePathz))
+for pathy in reversePathy_n:
+    ax[1, 1].plot(reverseN, pathy**2, "b--", linewidth=0.5)
+
+ax[1, 1].plot(reverseN, reversePathz**2,                                               "r")
+ax[1, 1].plot(reverseN, 1 - reversePathx**2 - reversePathy_total**2 - reversePathz**2, "g")
+ax[1, 1].plot(reverseN, reversePathx**2 + reversePathy_total**2,                       "b")
+
+ax[0, 1].plot(reverseN, accelerationExpression(reversePathx, reversePathy_total, reversePathz))
+
+##
 
 fixedPoints = fixedPoints.transpose()
 
